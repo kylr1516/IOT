a6e235020c7a59d92d7df92ddc7cce8cd5088818
Kyle Myers <kyle2004@byu.edu>
2023-02-01 10:38:08 -0700

    finished hourly version

--- trafficaq/finddates hourly.py
+++ trafficaq/finddates hourly.py
@@ -2,7 +2,7 @@
 
 import csv
 
-def opener(file,col):
+def opener(file,col):#opens the csv file
     path = file
     lines=[]
     with open(path, newline='') as csvfile:
@@ -11,19 +11,35 @@ def opener(file,col):
             lines.append([row[0],row[col]])
     lines=lines[1:]
     return lines
-#print(data[0][0:15]) ##this goes [which line][how many characters]--want last to be comma seperated sometimes 
+#print(opener("2021-PM2.5.csv",2)[:10]) 
 
 
-def gapfinder(file,diff=30,mod=True,col=2):
+def gapfinder(file,diff=30,col=2):#returns all the times for the given column where the diff was reached
     data = opener(file,col)
     dates=[]
-    #data=[line.split(",") for line in data]
-    previousday=data[1][1]
-    print("first day check:",previousday)
+    missing=[]
+    previousday=data[0][1]
+    #print("first day check:",previousday)
     for line in data:
-        if abs(eval(previousday)-eval(line[1])) >=diff:
-            if line[0] not in dates:
-                dates.append(line[0])
-        previousday=line[1]
+        try:
+            if abs(eval(previousday)-eval(line[1])) >=diff:
+                if line[0] not in dates:
+                    dates.append(line[0])
+            previousday=line[1]
+        except SyntaxError:
+            missing.append(line[0])
     return dates
-(gapfinder('2021-PM2.5.csv'))
+#print(gapfinder('2021-PM2.5.csv'))
+
+def time_dictionary(file,cols,diff=30):#returns dict of all the time and dates at which the diff was surpassed and how many stations also recorded that diff
+    all_times={}
+    for i in range(1,cols+1):
+        dates=(gapfinder(file,diff=diff,col=i))
+        for j in range(len(dates)):
+            if dates[j] not in all_times:
+                all_times[dates[j]]=str(i)
+            else:
+                all_times[dates[j]]+=','
+                all_times[dates[j]]+=(str(i))
+    return all_times
+print(time_dictionary('2021-PM2.5.csv',22))
