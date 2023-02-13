3173c5467ea31a7819f17b8ba4c1845e54e9781b
Kyle Myers <kyle2004@byu.edu>
2023-02-13 11:27:54 -0700

    clean

--- trafficaq/finddates hourly.py
+++ trafficaq/finddates hourly.py
@@ -1,3 +1,5 @@
+#prbly don't use this due to the deq hourly data being a mess of abreviations and unknowns and inconsistency between their own sites
+
 import csv
 
 def opener(file,col):#opens the csv file
@@ -9,33 +11,34 @@ def opener(file,col):#opens the csv file
             lines.append([row[0],row[col]])
     return lines
 
-#cols=BV-MC,CV-MC,ED-MC,EN-MC,H3-MC,H3-MC-CO,HA-MC,HC-MC,HV-MC,HW-MC,IP-MC,LN-MC,NR-MC,RP-MC,RS-MC,RS-MC-CO,SF-MC,!!!SM-MC-CO,SM-MC-CO2,UT-MC,UT-MC-CO,V4-MC (starting at 1)
-#for some reason some end in -MC-CO or -MC-CO2 rather than just -MC while having the same site abreviation
-#to acces these use the site ID then .1 for -MC and .2 for -MC-CO and .3 for -MC-CO2
-#also I have no idea what UT would stand for except average of Utah and it doesn't work to the average with the provided stations
-#also HA,IP appears to not exist
+#cols=BV-MC,CV-MC,ED-MC,EN-MC,H3-MC,H3-MC-CO,HA-MC,HC-MC,HV-MC,HW-MC,IP-MC,LN-MC,NR-MC,RP-MC,RS-MC,RS-MC-CO,SF-MC,SM-MC-CO,SM-MC-CO2,UT-MC,UT-MC-CO,V4-MC (starting at 1)
+#found what mc,co,and co2 stand for (I think; it is never explicity stated and not well supported but better than co)
+#mc is the main monitor, co is the colocated monitor, and co2 is the 2nd colocated monitor
+#to acces these use the site ID then regular for -MC and .2 for -MC-CO and .3 for -MC-CO2
+#however, there is no consistency in the labeling so that there is a single factor that makes a co station
+#also HA appears to not exist
 #there are also sites listed on the web with valid sensors that are not included in this list
 coldict={490110004:1,
     490352005:2,
     490450004:3,
     490210005:4,
-    490353013.1:5,
+    490353013:5,
     490353013.2:6,
     0.1:7,
     490530007:8,
     490571003:9,
     490353006:10,
-    0.2:11,
+    490353016:11,
     490494001:12,
     490354002:13,
     490353010:14,
-    490130002.1:15,
+    490130002:15,
     490130002.2:16,
     490495010:17,
     490050007.2:18,
     490050007.3:19,
-    0.3:20,
-    0.4:21,
+    490353015:20,
+    490353015.2:21,
     490471004:22,
     }
 
@@ -64,7 +67,7 @@ def gapfinder(file,diff=30,col=2):#returns all the times for the given column wh
             previousday=line[1]
         except SyntaxError:
             missing.append(line[0])
-    # print("# of dates missing",len(missing))
+    print("# of dates missing",len(missing))
     return dates
 #print(gapfinder('2021-PM2.5.csv'))
 
@@ -81,6 +84,6 @@ def time_dictionary(file,cols=22,diff=30):#returns dict of all the time and date
     return all_times
 # print(time_dictionary('2021-PM2.5.csv'))
 time_dic=time_dictionary('2021-PM2.5.csv')
-for date in (time_dic):
-    print(date)
-    print(len(time_dic[date]))
+#for date in (time_dic):
+#    print(date)
+#    print(len(time_dic[date]))
---
+++ trafficaq/mounter/mount.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+#This is meant to download, mount, and retrive information from a sqsh file
+read -p "Camera number: " CAMERA
+
+
+# Making sure the proper directories exist
+if ! test -e "$PWD/mnt"
+then
+mkdir -p "$PWD/mnt"
+fi
+if ! test -e "$PWD/mnt/$CAMERA"
+then
+mkdir -p "$PWD/mnt/$CAMERA"
+fi
+if ! test -e "$PWD/sqsh"
+then
+mkdir -p "$PWD/sqsh"
+fi
+if ! test -e "$PWD/images"
+then
+mkdir -p "$PWD/images"
+fi
+
+DOWNLOAD_TO="${PWD}/sqsh"
+
+#Only download if it isn't already on my computer
+if test -e "${DOWNLOAD_TO}/$CAMERA.sqsh"
+then
+    echo "$CAMERA.sqsh is already on local device"
+else
+    #Getting the credentials required to download from cadem
+    read -p "CADEM account: " USERNAME
+    DOWNLOAD_TO="${PWD}/sqsh"
+    DOWNLOAD_FROM="$USERNAME@ssh.et.byu.edu:/fsc/$USERNAME/groups/aq-images/images/$CAMERA.sqsh"
+
+    #The actual download happens here
+    echo "Downloading $CAMERA"
+    scp $DOWNLOAD_FROM $DOWNLOAD_TO
+fi
+
+
+
+
+#TODO: only mount if something else isn't mounted
+
+#mounting the sqsh file
+echo "Mounting $CAMERA"
+mount -o loop "$DOWNLOAD_TO/$CAMERA.sqsh" "$PWD/mnt/$CAMERA"
+
+#Finding and choosing the files to download
+echo "\nYears avalible:"
+ls "$PWD/mnt/$CAMERA"
+read -p "Year: " YEAR
+
+echo "\nMonths avalible:"
+ls "$PWD/mnt/$CAMERA/$YEAR"
+read -p "Month: " MONTH
+
+echo "\nDays avalible:"
+ls "$PWD/mnt/$CAMERA/$YEAR/$MONTH"
+read -p "Day: " DAY
+
+#Clearning up the image folder so that it doesn't get crouded upon multiple itterations
+rm -r "$PWD/images"
+mkdir -p "$PWD/images/$YEAR/$MONTH/$DAY"
+
+#Copying the files over and making sure they have the correct permisions so that we can view them
+cp -av "$PWD/mnt/$CAMERA/$YEAR/$MONTH/$DAY/." "$PWD/images/$YEAR/$MONTH/$DAY/"
+chown -R carter:carter "$PWD/images/"
+chmod 660 "$PWD/images/$YEAR/$MONTH/$DAY/"*
+
+
+#Unmounting and cleaning up mnt
+umount "$PWD/mnt/$CAMERA"
+rmdir "$PWD/mnt/$CAMERA"
