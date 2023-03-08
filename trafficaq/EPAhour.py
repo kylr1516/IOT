f34cd21cf9d286eca39822274edb46d570676658
Kyle Myers <kyle2004@byu.edu>
2023-03-08 12:13:01 -0700

    added hourly csv maker

--- trafficaq/EPAhour.py
+++ trafficaq/EPAhour.py
@@ -14,6 +14,19 @@ def opener(file,state): #Utah is state code 49
             elif row[0]==state:
                 lines.append([state+row[1]+row[2],row[5],row[6],row[9]+' '+row[10],row[13]])
     return lines #output is [epa station code,lat,long,date+time,pm2.5 val]
+def opener2(file,state): #Utah is state code 49
+    path = file
+    lines=[]
+    with open(path, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        rownum=0
+        for row in reader:
+            if rownum==0:
+                lines=[]
+                rownum+=1
+            elif row[0]==state:
+                lines.append([state+row[1]+row[2],row[5],row[6],row[9],row[10],row[13]])
+    return lines #output is [epa station code,lat,long,date,time,pm2.5 val]
 # print(opener('hourly_88101_2022.csv','49')[-15:]) 
 
 def firstvalue(data):
@@ -51,7 +64,7 @@ def gapfinder(file,state='49',diff=30,stationcode=0):#may run errors when reachi
             missing.append(line[-2])
     print("# of dates missing",len(missing))
     return dates
-print(gapfinder('hourly_88101_2021.csv',stationcode='490353013'))
+# print(gapfinder('hourly_88101_2021.csv',stationcode='490353013'))
 
 
 def site_csv_maker(file,stationcode,writefile,state='49'):
@@ -68,6 +81,22 @@ def site_csv_maker(file,stationcode,writefile,state='49'):
         sys.stdout=orig_std_out
 # site_csv_maker('hourly_88101_2021.csv','490353013','490353013.csv')
 
+def site_csv_hour_maker(file,stationcode,writefile,hours,state='49'):
+    rawdata = opener2(file,state)
+    data=[]
+    for line in rawdata:
+        if line[0]==stationcode:
+            for hour in hours:
+                if line[-2]==hour:
+                    data.append(line)
+    orig_std_out= sys.stdout
+    with open(writefile,'w') as f:
+        sys.stdout=f
+        for lines in data:
+            print(lines)
+        sys.stdout=orig_std_out
+
+site_csv_hour_maker('hourly_88101_2021.csv','490354002','490354002.csv',['12:00','09:00'])
 
 # stations of note= 490353013, 490450004, 490494001, 490353016, 490495010, 490353013, 490352005, 490353015, 490494002
 #most of these spikes occured in august and on the 6th and 18th
